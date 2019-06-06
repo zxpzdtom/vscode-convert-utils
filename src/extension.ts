e91dbb2a7b005df30d539123d398e3b3aa27b803
wb-cs555011 <wb-cs555011@koubei.com>
2019-06-06 18:57:23 +0800

    ts

--- src/extension.ts
+++ src/extension.ts
@@ -10,11 +10,11 @@ export function activate(context: vscode.ExtensionContext) {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) { return; }
 
-		editor.edit(builder => {
-			editor.selections.forEach(selection => {
+		editor.edit((builder: { replace: (arg0: any, arg1: string) => void; }) => {
+			editor.selections.forEach((selection: any) => {
 				let text = editor.document.getText(selection);
 				let arr: string[] = [];
-				text.split('\n').forEach(item => {
+				text.split('\n').forEach((item: string) => {
 					// 去除左右空格、下划线、连接符 中文
 					item = item.replace(new RegExp('^[\u4e00-\u9fa5 _-]+|[\u4e00-\u9fa5]|[\u4e00-\u9fa5 _-]+$', 'g'), '');
 					if (/ /g.test(item)) { // 含空格 => foot_bar
@@ -23,14 +23,14 @@ export function activate(context: vscode.ExtensionContext) {
 						item = item.toUpperCase();
 					} else if (/(?!.*[a-z]).+$/.test(item)) { // 字母全大写
 						let temp = item.split(/[- _]/).map( // S纪念馆12-JNJDG_S S123 => ["S纪念馆12", "JNJDG", "S", "S123"]
-							s => s.charAt(0).toUpperCase() + s.slice(1).toLowerCase() // ["S纪念馆12", "Jnjdg", "S", "S123"]
+							(s: { charAt: (arg0: number) => { toUpperCase: () => string | number; }; slice: (arg0: number) => { toLowerCase: () => string | number; }; }) => `${s.charAt(0).toUpperCase()}${s.slice(1).toLowerCase()}` // ["S纪念馆12", "Jnjdg", "S", "S123"]
 						).join('');
 						item = temp.charAt(0).toLowerCase() + temp.slice(1); // s纪念馆12JnjdgSS123
 					} else if (/^[^A-Z](?!.*[- _]).+$/.test(item)) { // 驼峰 => foot-bar
 						item = item.replace(/([A-Z])/g, '-$1').replace(/[-_\s]+/g, '-').toLowerCase();
 					} else if (/^(?=.*-)(?!.*[_ ]).+$/.test(item)) { // foot-bar => FootBar
-						item = item.split('-').map(s =>
-							s.charAt(0).toUpperCase() + s.slice(1).toLowerCase() // 首字母大写
+						item = item.split('-').map((s: { charAt: (arg0: number) => { toUpperCase: () => string | number; }; slice: (arg0: number) => { toLowerCase: () => string | number; }; }) =>
+							`${s.charAt(0).toUpperCase()}${s.slice(1).toLowerCase()}` // 首字母大写
 						).join('')
 					} else if (/^[A-Z](?!.*[_ -]).+$/.test(item)) {
 						//  匹配全小写 至少一个 - 但不含  _ 和 空格
